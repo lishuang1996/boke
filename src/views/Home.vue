b6924b761c2177bfd605d860e015f8795ec41fb1
ls“lsgit config --global user.email“your@email.com <437845145@qq.com>
2019-05-17 16:18:49 +0800

    11

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="">
-    111
+  <div class="Home_Page">
+    <div class="rock_bottom_bg">
+      <img src="../assets/home/bg.jpg" alt="">
+    </div>
+    <div class="superstratum">
+
+    </div>
   </div>
 </template>
 
@@ -13,7 +18,29 @@ export default {
 
   },
   created(){
-    console.log(111)
+    
   }
 }
 </script>
+<style lang="scss" scoped>
+.Home_Page{
+  width: 100%;
+}
+.rock_bottom_bg{
+  background-color: bisque;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  z-index: -1;
+  display: flex;
+  justify-content:center;
+}
+.superstratum{
+  height: 1200px;
+  position: relative;
+  top: 1000px;
+  width: 100%;
+  background-color: #fff;
+}
+</style>
+
